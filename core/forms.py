5a4a61a8d96cd98aa1a9c2183664fcdd71024dd8
Parsa Ghaderi <pghaderi@cisco.com>
2025-07-31 18:40:06 -0400

    reject dups

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Hobby, Profile, Requirement, Tag, Category
+from django.core.exceptions import ValidationError
+import uuid
 
 class HobbyForm(forms.ModelForm):
     new_category = forms.CharField(required=False, label="Add New Category")
@@ -19,6 +21,23 @@ class HobbyForm(forms.ModelForm):
             'tags': forms.CheckboxSelectMultiple,
             'date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
         }
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if Hobby.objects.filter(title=title).exists():
+            raise ValidationError("A hobby/event with this name already exists. Please choose a different name.")
+        return title
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        image = self.cleaned_data.get('image', None)
+        if image:
+            ext = image.name.split('.')[-1]
+            image.name = f"hobby_{uuid.uuid4().hex}.{ext}"
+        if commit:
+            instance.save()
+            self.save_m2m()
+        return instance
 
 class ProfileForm(forms.ModelForm):
     class Meta:
