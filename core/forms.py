3bcf381850422ddbec70afb8dca427db9301d5d4
Parsa Ghaderi <pghaderi@cisco.com>
2025-09-12 12:39:29 -0400

    adding the userupdateform and profileupdateform

--- core/forms.py
+++ core/forms.py
@@ -71,3 +71,16 @@ class SupplierUserCreationForm(UserCreationForm):
                 neighbourhood=self.cleaned_data.get('neighbourhood','')
             )
         return user
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['bio', 'goal', 'image']
