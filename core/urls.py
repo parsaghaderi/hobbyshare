8d21e2338213a750fefb4a213216fd51f97ab0b8
Parsa Ghaderi <pghaderi@cisco.com>
2025-09-12 13:58:16 -0400

    remove about

--- core/urls.py
+++ core/urls.py
@@ -31,6 +31,5 @@ urlpatterns = [
     path('requirement/<int:req_id>/delete/', views.delete_requirement, name='delete_requirement'),
     path('api/supplier-item-suggestions/', views.supplier_item_suggestions, name='supplier_item_suggestions'),
     path('supplier/requirement/<int:req_id>/<str:decision>/', views.supplier_decide_requirement, name='supplier_decide_requirement'),
-    path('hobby/requirement/<int:req_id>/link-supplier/', views.host_link_supplier_item, name='host_link_supplier_item'),
-    path('about/', about_page, name='about'),
+    path('hobby/requirement/<int:req_id>/link-supplier/', views.host_link_supplier_item, name='host_link_supplier_item')
 ]
