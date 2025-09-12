3bee851bea10a0b56f258e6764770cc730ae5e1b
Parsa Ghaderi <pghaderi@cisco.com>
2025-09-12 13:13:05 -0400

    simple profile pic

--- core/models.py
+++ core/models.py
@@ -19,27 +19,14 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True)
     goal = models.CharField(max_length=255, blank=True)
+    # This is the simple, correct way, just like your Hobby model.
     image = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
 
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    # Add this save method to resize images
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-
-        if self.image and hasattr(self.image, 'path'):
-            try:
-                img = Image.open(self.image.path)
-
-                # Resize logic
-                if img.height > 300 or img.width > 300:
-                    output_size = (300, 300)
-                    img.thumbnail(output_size)
-                    img.save(self.image.path)
-            except (IOError, FileNotFoundError):
-                # Ignore if file is missing or corrupt
-                pass
+    # The custom save method has been removed to prevent file handling errors.
+    # Django and the database will handle saving the image path directly.
 
     def get_host_rating(self):
         ratings = Rating.objects.filter(hobby__host=self.user)
