5efe9d0f2d22118d86aaec284ed2e677a8a1e1d5
Parsa Ghaderi <pghaderi@cisco.com>
2025-09-30 12:23:38 -0400

    rollback to v2

--- core/forms.py
+++ core/forms.py
@@ -3,6 +3,12 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Hobby, Profile
 
+class SupplierUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=False)
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
 class HobbyForm(forms.ModelForm):
     class Meta:
         model = Hobby
