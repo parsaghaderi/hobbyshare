fe3d5c79868cf8b29d6a4966639d75120d84c59c
Parsa Ghaderi <pghaderi@cisco.com>
2025-09-30 12:13:48 -0400

    adding schedule; fix

--- core/admin.py
+++ core/admin.py
@@ -1,24 +1,8 @@
 from django.contrib import admin
-from .models import (
-    Profile, Hobby, Category, Tag, Application, Rating,
-    Requirement, SupplierItem, SupplierProfile
-)
+from .models import Profile, Hobby, Category, Tag, Application, Rating
 
-# Unregister models if they are already registered to avoid errors on reload
-# This part is for safety, in case of complex setups.
-# You can likely remove this block if it causes issues, but it's good practice.
-try:
-    admin.site.unregister(Profile)
-    admin.site.unregister(Hobby)
-    admin.site.unregister(Category)
-    admin.site.unregister(Tag)
-    admin.site.unregister(Application)
-    admin.site.unregister(Rating)
-    admin.site.unregister(Requirement)
-    admin.site.unregister(SupplierItem)
-    admin.site.unregister(SupplierProfile)
-except admin.sites.NotRegistered:
-    pass
+# This is a minimal, safe admin configuration that only registers
+# models confirmed to exist in your project.
 
 
 @admin.register(Hobby)
@@ -59,21 +43,3 @@ class ApplicationAdmin(admin.ModelAdmin):
 class RatingAdmin(admin.ModelAdmin):
     list_display = ('hobby', 'user', 'score', 'created_at')
     list_filter = ('score',)
-
-
-@admin.register(Requirement)
-class RequirementAdmin(admin.ModelAdmin):
-    list_display = ('hobby', 'description', 'is_claimed', 'claimed_by')
-    list_filter = ('is_claimed',)
-
-
-@admin.register(SupplierItem)
-class SupplierItemAdmin(admin.ModelAdmin):
-    list_display = ('name', 'supplier', 'price')
-    search_fields = ('name', 'description')
-
-
-@admin.register(SupplierProfile)
-class SupplierProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'company_name')
-    search_fields = ('user__username', 'company_name')
