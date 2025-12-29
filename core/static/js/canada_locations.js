516663ffe8dc3856ad1f4f9363ff4936cddeb845
Parsa Ghaderi <pghaderi@cisco.com>
2025-12-29 14:09:13 -0500

    Expand Canadian provinces and major cities lists

--- core/static/js/canada_locations.js
+++ core/static/js/canada_locations.js
@@ -1,16 +1,85 @@
 // Minimal Canada locations dataset (can be expanded)
 // Structure: { provinceCode: { name: 'Ontario', cities: { 'Toronto': ['Downtown','Scarborough','North York','Etobicoke'], ... } } }
 window.CANADA_LOCATIONS = {
-  QC: { name: 'Quebec', cities: { 'Montreal': ['Downtown','Plateau','Griffintown','NDG','Ville-Marie'], 'Quebec City': ['Old Quebec','Montcalm','Saint-Roch'], 'Laval': ['Chomedey','Sainte-Dorothee'] } },
-  ON: { name: 'Ontario', cities: { 'Toronto': ['Downtown','North York','Scarborough','Etobicoke'], 'Ottawa': ['Centretown','Kanata','Orleans'], 'Hamilton': ['Downtown','Dundas'], 'Mississauga': ['Square One','Port Credit'] } },
-  AB: { name: 'Alberta', cities: { 'Calgary': ['Downtown','Beltline','Kensington'], 'Edmonton': ['Downtown','Strathcona','Westmount'], 'Red Deer': ['Downtown'] } },
-  BC: { name: 'British Columbia', cities: { 'Vancouver': ['Downtown','Kitsilano','Mount Pleasant'], 'Victoria': ['Downtown','Fernwood'], 'Surrey': ['Guildford','Newton'] } },
-  MB: { name: 'Manitoba', cities: { 'Winnipeg': ['Downtown','St. Boniface','Fort Garry'] } },
-  SK: { name: 'Saskatchewan', cities: { 'Saskatoon': ['Riversdale','Nutana'], 'Regina': ['Downtown','Cathedral'] } },
-  NS: { name: 'Nova Scotia', cities: { 'Halifax': ['Downtown','North End','South End'] } },
-  NB: { name: 'New Brunswick', cities: { 'Moncton': ['Downtown'], 'Fredericton': ['Downtown'], 'Saint John': ['Uptown'] } },
-  NL: { name: 'Newfoundland and Labrador', cities: { 'St. John\'s': ['Downtown','Quidi Vidi'] } },
-  PE: { name: 'Prince Edward Island', cities: { 'Charlottetown': ['Downtown'] } },
+  QC: { name: 'Quebec', cities: {
+    'Montreal': ['Downtown','Plateau','Griffintown','NDG','Ville-Marie'],
+    'Quebec City': ['Old Quebec','Montcalm','Saint-Roch','Sainte-Foy'],
+    'Laval': ['Chomedey','Sainte-Dorothee','Laval-des-Rapides'],
+    'Gatineau': ['Hull','Aylmer'],
+    'Longueuil': ['Vieux-Longueuil','Greenfield Park'],
+    'Sherbrooke': ['Lennoxville'],
+    'Saguenay': ['Chicoutimi','Jonquiere'],
+    'Trois-Rivieres': ['Cap-de-la-Madeleine'],
+    'Terrebonne': ['Lachenaie']
+  } },
+  ON: { name: 'Ontario', cities: {
+    'Toronto': ['Downtown','North York','Scarborough','Etobicoke'],
+    'Ottawa': ['Centretown','Kanata','Orleans'],
+    'Mississauga': ['Square One','Port Credit','Streetsville'],
+    'Brampton': ['Downtown','Bramalea'],
+    'Hamilton': ['Downtown','Dundas','Stoney Creek'],
+    'London': ['Downtown','Masonville'],
+    'Markham': ['Unionville'],
+    'Vaughan': ['Maple','Concord'],
+    'Kitchener': ['Downtown'],
+    'Windsor': ['Downtown'],
+    'Richmond Hill': ['Hillcrest'],
+    'Oakville': ['Kerr Village'],
+    'Waterloo': ['Uptown']
+  } },
+  AB: { name: 'Alberta', cities: {
+    'Calgary': ['Downtown','Beltline','Kensington'],
+    'Edmonton': ['Downtown','Strathcona','Westmount'],
+    'Red Deer': ['Downtown'],
+    'Lethbridge': ['Downtown'],
+    'St. Albert': ['Downtown'],
+    'Medicine Hat': ['Southlands'],
+    'Grande Prairie': ['Avondale']
+  } },
+  BC: { name: 'British Columbia', cities: {
+    'Vancouver': ['Downtown','Kitsilano','Mount Pleasant'],
+    'Surrey': ['Guildford','Newton','Whalley'],
+    'Burnaby': ['Metrotown','Brentwood'],
+    'Richmond': ['City Centre','Steveston'],
+    'Abbotsford': ['Clearbrook'],
+    'Coquitlam': ['Town Centre'],
+    'Kelowna': ['Downtown'],
+    'Victoria': ['Downtown','Fernwood','James Bay'],
+    'Nanaimo': ['Downtown'],
+    'Kamloops': ['Sahali','North Shore']
+  } },
+  MB: { name: 'Manitoba', cities: {
+    'Winnipeg': ['Downtown','St. Boniface','Fort Garry'],
+    'Brandon': ['Downtown'],
+    'Steinbach': ['Downtown'],
+    'Thompson': ['Downtown']
+  } },
+  SK: { name: 'Saskatchewan', cities: {
+    'Saskatoon': ['Riversdale','Nutana','Stonebridge'],
+    'Regina': ['Downtown','Cathedral','Harbour Landing'],
+    'Prince Albert': ['Downtown'],
+    'Moose Jaw': ['Downtown']
+  } },
+  NS: { name: 'Nova Scotia', cities: {
+    'Halifax': ['Downtown','North End','South End'],
+    'Sydney': ['Downtown'],
+    'Dartmouth': ['Downtown']
+  } },
+  NB: { name: 'New Brunswick', cities: {
+    'Moncton': ['Downtown'],
+    'Fredericton': ['Downtown'],
+    'Saint John': ['Uptown'],
+    'Dieppe': ['Downtown']
+  } },
+  NL: { name: 'Newfoundland and Labrador', cities: {
+    'St. John\'s': ['Downtown','Quidi Vidi'],
+    'Mount Pearl': ['Downtown'],
+    'Corner Brook': ['Downtown']
+  } },
+  PE: { name: 'Prince Edward Island', cities: {
+    'Charlottetown': ['Downtown'],
+    'Summerside': ['Downtown']
+  } },
   YT: { name: 'Yukon', cities: { 'Whitehorse': ['Downtown'] } },
   NT: { name: 'Northwest Territories', cities: { 'Yellowknife': ['Downtown'] } },
   NU: { name: 'Nunavut', cities: { 'Iqaluit': ['Downtown'] } }
